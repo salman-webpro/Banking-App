943c7d0312d9bbc5a10df09225df9b1dc7dbdd94
Salman Mahmud <smahmud295@gmail.com>
2024-11-22 15:29:54 +0600

    push

--- script.js
+++ script.js
@@ -79,14 +79,3 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
