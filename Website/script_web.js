e7ea9f753d09cc7f065102802a0ee01b40f4b86d
Salman Mahmud <smahmud295@gmail.com>
2024-12-12 17:50:05 +0600

    some changes

--- Website/script_web.js
+++ Website/script_web.js
@@ -7,9 +7,13 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const learnMoreBtn = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
 
 // open Modal
 const openModal = function () {
@@ -59,7 +63,6 @@ tabsContainer.addEventListener("click", function (e) {
   tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
   // adding classes
   clicked.classList.add("operations__tab--active");
-  console.log(clicked.dataset.tab);
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
@@ -111,7 +114,7 @@ const revealObs = new IntersectionObserver(revealObsCallback, {
 
 sections.forEach((section) => {
   revealObs.observe(section);
-  section.classList.add("section--hidden");
+  // section.classList.add('section--hidden');
 });
 
 // lazy loading images
@@ -119,7 +122,6 @@ const images = document.querySelectorAll("img[data-src]");
 
 const imagesObsCallback = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.src = entry.target.dataset.src;
   entry.target.addEventListener("load", () =>
@@ -136,74 +138,77 @@ const imagesObs = new IntersectionObserver(imagesObsCallback, {
 images.forEach((img) => imagesObs.observe(img));
 
 // Slider
-const slides = document.querySelectorAll(".slide");
-const slider = document.querySelector(".slider");
-let currSlide = 0;
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const sliderContainer = document.querySelector(".slider");
+  let currSlide = 0;
+  const dots = document.querySelector(".dots");
 
-// slider changing function
-const changeSlide = function (slide) {
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  // slider changing function
+  const changeSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+  };
+
+  const dotsChanging = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((s) => s.classList.remove("dots__dot--active"));
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const nextSlide = function () {
+    currSlide === slides.length - 1 ? (currSlide = 0) : currSlide++;
+    changeSlide(currSlide);
+    dotsChanging(currSlide);
+  };
+
+  const previousSlide = function () {
+    currSlide === 0 ? (currSlide = slides.length - 1) : currSlide--;
+    changeSlide(currSlide);
+    dotsChanging(currSlide);
+  };
+  // slider button actions
+  sliderContainer.addEventListener("click", function (e) {
+    // next button click
+    if (e.target.classList.contains("slider__btn--right")) nextSlide();
+    // previous button click
+    if (e.target.classList.contains("slider__btn--left")) previousSlide();
+  });
+
+  // slider change on keypress
+  document.addEventListener("keydown", function (e) {
+    e.key === "ArrowLeft" && previousSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  // slider change pagination dots
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dots.insertAdjacentHTML(
+        "beforeend",
+        `<button class='dots__dot' data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  // default behavior
+  const init = function () {
+    createDots();
+    changeSlide(0);
+    dotsChanging(0);
+  };
+  init();
+
+  sliderContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const clickedDot = e.target.dataset.slide;
+      changeSlide(clickedDot);
+      dotsChanging(clickedDot);
+    }
   });
 };
-
-const dotsChanging = function (slide) {
-  document
-    .querySelectorAll(".dots__dot")
-    .forEach((s) => s.classList.remove("dots__dot--active"));
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add("dots__dot--active");
-};
-
-const nextSlide = function () {
-  currSlide === slides.length - 1 ? (currSlide = 0) : currSlide++;
-  changeSlide(currSlide);
-  dotsChanging(currSlide);
-};
-
-const previousSlide = function () {
-  currSlide === 0 ? (currSlide = slides.length - 1) : currSlide--;
-  changeSlide(currSlide);
-  dotsChanging(currSlide);
-};
-// slider button actions
-slider.addEventListener("click", function (e) {
-  // next button click
-  if (e.target.classList.contains("slider__btn--right")) nextSlide();
-  // previous button click
-  if (e.target.classList.contains("slider__btn--left")) previousSlide();
-});
-
-// slider change on keypress
-document.addEventListener("keydown", function (e) {
-  e.key === "ArrowLeft" && previousSlide();
-  e.key === "ArrowRight" && nextSlide();
-});
-
-// slider change pagination dots
-const dots = document.querySelector(".dots");
-const createDots = function () {
-  slides.forEach((_, i) => {
-    dots.insertAdjacentHTML(
-      "beforeend",
-      `<button class='dots__dot' data-slide="${i}"></button>`
-    );
-  });
-};
-
-// default behavior
-const init = function () {
-  createDots();
-  changeSlide(0);
-  dotsChanging(0);
-};
-init();
-
-slider.addEventListener("click", function (e) {
-  if (e.target.classList.contains("dots__dot")) {
-    const clickedDot = e.target.dataset.slide;
-    changeSlide(clickedDot);
-    dotsChanging(clickedDot);
-  }
-});
+slider();
